344725f652a3743dc0152bcf9aeba14ac7d43a83
Muqeet-Ryan <muqeetryan@outlook.com>
2026-02-08 15:20:39 +0600

    frontend setup

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+
+export const api = axios.create({
+    baseURL: import.meta.env.MODE === 'development' ? 'http://localhost:5002/api' : '/api',
+    withCredentials: true
+});
