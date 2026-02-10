4486138e41a4f4f86d757e4e17acbb5917ee539a
Muqeet-Ryan <muqeetryan@outlook.com>
2026-02-10 11:39:27 +0600

    signupPage UI is functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,39 @@
-import { Routes,Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { useEffect } from "react";
+import {Toaster} from 'react-hot-toast'
+
 const App = () => {
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log(authUser);
+
   return (
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
-       <Routes>
-           <Route path="/signup" element={<SignUpPage/>} />
-           <Route path="/login" element={<LoginPage/>} />
-           <Route path="/" element={<ChatPage/>} />
-      </Routes>
-  </div>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}
+        />
 
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+        />
+      </Routes>
+      <Toaster/>
+    </div>
   );
 };
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,38 @@
 import {create} from 'zustand';
+import {api} from '../lib/axios.js'
+import SignUpPage from '../pages/SignUpPage.jsx';
+import toast from 'react-hot-toast';
 
-export const useAuthStore = create(() => ({
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isCheckingAuth: true,
+  isSingingUp: false,
+
+  checkAuth: async() => {
+    try {
+      const res = await api.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.error(error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async(data) => {
+    set({isSingingUp: true});
+    try {
+      const res = await api.post('/auth/signup', data);
+      set({authUser: res.data});
+      toast.success('user created');
+    } catch (error) {
+      console.error('Error in signup', error);
+      toast.error('Error signing-up');
+    } finally {
+      set({isSingingUp: false});
+    }
+  },
 
 }));
+
